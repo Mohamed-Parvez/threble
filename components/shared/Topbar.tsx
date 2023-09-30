9bd6a1d159702874cd8a93d2703a5a565336f987
muaath-rifath <110122106016@aalimec.ac.in>
2023-09-30 21:14:32 +0530

    modified:   app/(root)/layout.tsx modified:   app/favicon.ico modified:   components/shared/LeftSidebar.tsx modified:   components/shared/Topbar.tsx new file:   constants/index.js new file:   public/assets/community.svg new file:   public/assets/create.svg new file:   public/assets/delete.svg new file:   public/assets/edit.svg new file:   public/assets/heart-filled.svg new file:   public/assets/heart-gray.svg new file:   public/assets/heart.svg new file:   public/assets/home.svg new file:   public/assets/login.svg new file:   public/assets/logo.svg new file:   public/assets/logout.svg new file:   public/assets/members.svg new file:   public/assets/more.svg new file:   public/assets/profile.svg new file:   public/assets/reply.svg new file:   public/assets/repost.svg new file:   public/assets/request.svg new file:   public/assets/search-gray.svg new file:   public/assets/search.svg new file:   public/assets/share.svg new file:   public/assets/tag.svg new file:   public/assets/user.svg

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,11 +2,18 @@ import Link from "next/link"
 import Image from "next/image"
 
 function Topbar(){
+    const isUserLoggedIn = false;
     return( <nav className="topbar">
-        <Link className="flex items-center gap-4">
-            <Image src="/logo.svg" alt="logo" className="w-8 h-8"/>
+        <Link className="flex items-center gap-4" href="/">
+            <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
             <p className="text-heading3-bold text-light-1 max-xs:hidden">Threble</p>
         </Link>
+        <div className="flex items-center gap-1">
+            <div className="block md:hidden">
+                {isUserLoggedIn ? (<button className="btn btn-icon btn-circle btn-sm btn-ghost-light-1"><Image src="/assets/login.svg" alt="logout" width={24} height={24} /></button>):(<button className="btn btn-icon btn-circle btn-sm btn-ghost-light-1"><Image src="/assets/logout.svg" alt="logout" width={24} height={24} /></button>)}
+            </div>
+        </div>
+            
     </nav>)
 }
 export default Topbar
